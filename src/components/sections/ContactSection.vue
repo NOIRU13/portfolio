b2555d53dedf2b874d8452756cd464f3aa4078e0
jin kuroki <tr.ji.kuroki@gmail.com>
2025-09-23 03:59:35 +0900

    no message

--- src/components/sections/ContactSection.vue
+++ src/components/sections/ContactSection.vue
@@ -42,7 +42,7 @@
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                       </div>
-                      案件対応
+                      全国どこでも対応
                     </div>
                   </td>
                   <td class="px-6 py-4 text-sm text-gray-700">
